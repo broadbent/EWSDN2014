2095bbf65e5c7e29239c649ec1e7b45fe1c70a92
Matthew Broadbent <matt@matthewbroadbent.net>
2014-03-13 14:05:05 +0100

    Updated footer

--- include/footer.php
+++ include/footer.php
@@ -3,7 +3,7 @@
 	</div><!-- /content -->
 
 <div class="license">
-The <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">EWSDN 2014 Website</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>. Source code is available at <a href="https://github.com/ryanrhymes/SIGCOMM2012">github.com</a>.
+The <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">EWSDN 2014 Website</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>. Original design is available at <a href="https://github.com/ryanrhymes/SIGCOMM2012">github.com</a>.
 </div>
 
 </div><!-- /page -->
